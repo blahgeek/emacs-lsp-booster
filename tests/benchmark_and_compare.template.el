5a879df2d61cd3d220cadeb6cf0d27abb838d2cf
Yikai Zhao <yikai@z1k.dev>
2023-12-29 23:59:32 +0800

    optimize benchmark_and_compare elisp code

--- tests/benchmark_and_compare.template.el
+++ tests/benchmark_and_compare.template.el
@@ -7,10 +7,17 @@
 (setq gc-cons-threshold most-positive-fixnum
       gc-cons-percentage 0.6)
 
-(defun -all? (fn elems)
+(defun -all (fn elems)
   (let ((n (length elems))
         (all-vals (mapcar (lambda (x) (if (funcall fn x) 1 0)) elems)))
     (equal n (apply '+ all-vals))))
+
+(defun -plist-to-sorted-alist (p)
+  (let (res)
+    (while p
+      (push (cons (car p) (cadr p)) res)
+      (setq p (cddr p)))
+    (sort res (lambda (a b) (string< (symbol-name (car a)) (symbol-name (car b)))))))
 
 (defun json-equal (a b)
   (pcase a
@@ -24,19 +31,21 @@
     ((pred vectorp)
      (and (vectorp b)
           (equal (length a) (length b))
-          (-all? (lambda (idx) (json-equal (aref a idx) (aref b idx)))
+          (-all (lambda (idx) (json-equal (aref a idx) (aref b idx)))
                  (number-sequence 0 (1- (length a))))))
     ((pred plistp)
      (and (plistp b)
           (equal (length a) (length b))
-          (let (all-keys
-                (pa a))
-            (while pa
-              (push (car pa) all-keys)
-              (setq pa (cddr pa)))
-            (-all? (lambda (k)
-                     (json-equal (plist-get a k) (plist-get b k)))
-                   all-keys))))
+          (let ((a-alist (-plist-to-sorted-alist a))
+                (b-alist (-plist-to-sorted-alist b))
+                (res t))
+            (while a-alist
+              (unless (and (equal (caar a-alist) (caar b-alist))
+                           (json-equal (cdar a-alist) (cdar b-alist)))
+                (setq res nil))
+              (setq a-alist (cdr a-alist)
+                    b-alist (cdr b-alist)))
+            res)))
     (_ nil)))
 
 (let ((json-str (with-temp-buffer
