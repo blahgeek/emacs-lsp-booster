54853d6c66be785b647891a21f6653467db9cd61
Yikai Zhao <yikai@z1k.dev>
2024-01-01 22:24:45 +0800

    default log level; handle thread panic

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,18 @@ fn main() -> Result<()> {
         bail!("Usage: {} LSP_SERVER_CMD ARGS...", args[0]);
     }
 
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "info");
+    }
     env_logger::init();
+
+    // exit the process if any thread panic
+    let original_panic_hook = std::panic::take_hook();
+    std::panic::set_hook(Box::new(move |info| {
+        original_panic_hook(info);
+        std::process::exit(1);
+    }));
+
     let mut cmd = std::process::Command::new(&args[1]);
     cmd.args(&args[1..]);
 
