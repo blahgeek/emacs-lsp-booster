4714ca3c9b9fca6b8638525335a72a01161dcf2b
Yikai Zhao <yikai@z1k.dev>
2024-01-06 21:41:14 +0800

    Use clap to parse cli args

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,42 @@
 use anyhow::{Result, bail};
+use clap::Parser;
 
 use emacs_lsp_booster::app;
 
 
-fn main() -> Result<()> {
-    let args = std::env::args().collect::<Vec<_>>();
-    if args.len() < 2 {
-        bail!("Usage: {} LSP_SERVER_CMD ARGS...", args[0]);
-    }
+#[derive(Parser, Default)]
+#[command(long_about = None, about = None,
+          arg_required_else_help = true, after_help = "For backward compatibility, `emacs-lsp-booster <SERVER_CMD>...` (without any options) is also supported\n" )]
+struct Cli {
+    #[command(flatten)]
+    verbose: clap_verbosity_flag::Verbosity,
 
-    if std::env::var("RUST_LOG").is_err() {
-        std::env::set_var("RUST_LOG", "info");
+    #[arg(last = true)]
+    server_cmd: Vec<String>,
+}
+
+fn parse_args<T, S>(args: T) -> Cli
+where T: IntoIterator<Item=S>,
+      S: Into<String> {
+    let args = args.into_iter().map(|x| x.into()).collect::<Vec<String>>();
+    // backward compatible. support `emacs-lsp-booster server_cmd args...` directly
+    if args.len() > 1 && !args[1].starts_with('-') && !args.contains(&"--".into()) {
+        Cli {
+            server_cmd: args[1..].to_vec(),
+            ..Default::default()
+        }
+    } else {
+        Cli::parse_from(args)
     }
-    env_logger::init();
+}
+
+fn main() -> Result<()> {
+    let cli = parse_args(std::env::args());
+    env_logger::Builder::new().filter_level(cli.verbose.log_level_filter()).init();
+
+    if cli.server_cmd.is_empty() {
+        bail!("Please specify the server command");
+    }
 
     // exit the process if any thread panic
     let original_panic_hook = std::panic::take_hook();
@@ -21,9 +45,22 @@ fn main() -> Result<()> {
         std::process::exit(1);
     }));
 
-    let mut cmd = std::process::Command::new(&args[1]);
-    cmd.args(&args[2..]);
+    let mut cmd = std::process::Command::new(&cli.server_cmd[0]);
+    cmd.args(&cli.server_cmd[1..]);
 
     let exit_status = app::run_app_forever(std::io::stdin(), std::io::stdout(), cmd)?;
     std::process::exit(exit_status.code().unwrap_or(1))
+}
+
+#[test]
+fn test_parse_args() {
+    let cli = parse_args(vec!["emacs-lsp-booster", "server_cmd", "arg1"]);
+    assert_eq!(cli.server_cmd, vec!["server_cmd", "arg1"]);
+
+    let cli = parse_args(vec!["emacs-lsp-booster", "--", "server_cmd", "arg1"]);
+    assert_eq!(cli.server_cmd, vec!["server_cmd", "arg1"]);
+
+    let cli = parse_args(vec!["emacs-lsp-booster", "-v", "--", "server_cmd", "arg1"]);
+    assert_eq!(cli.verbose.log_level_filter(), log::LevelFilter::Warn);
+    assert_eq!(cli.server_cmd, vec!["server_cmd", "arg1"]);
 }
