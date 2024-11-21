979a64e72c52c1f87192ae80e86dd70066161ff2
WSRer <1749094641@qq.com>
2024-11-21 09:34:16 +0800

    simd-json

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use clap::Parser;
 
 use emacs_lsp_booster::app;
 use emacs_lsp_booster::bytecode;
+use mimalloc::MiMalloc;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
 
 
 #[derive(Parser)]
