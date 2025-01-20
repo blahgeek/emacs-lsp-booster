f249189e43ec3c635bc04e7e79c7e4d33163f64a
WSRer <1749094641@qq.com>
2025-01-20 10:23:26 +0800

    use mimalloc

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,10 @@ use clap::Parser;
 use emacs_lsp_booster::app;
 use emacs_lsp_booster::bytecode;
 
-#[global_allocator]
-static ALLOC: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
+use mimalloc::MiMalloc;
 
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
 
 #[derive(Parser)]
 #[command(long_about = None, about = None,
